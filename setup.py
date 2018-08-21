2ffa509cb61d6fe60ab51ecda5d4ca35cee62239
Adam Taranto <adam.p.taranto@gmail.com>
2018-08-20 17:37:48 -0700

    package config

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+pypi_classifiers = [
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    "Development Status :: 4 - Beta",
+    "Environment :: Console",
+    "Operating System :: OS Independent",
+    'Intended Audience :: Science/Research',
+    'Natural Language :: English',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    'License :: OSI Approved :: MIT License',
+]
+
+desc = """Filter SAM file for soft-clipped alignments containing unassembled telomeric repeats."""
+
+setup(name='teloclip',
+      version='0.0.1',
+      description=desc,
+      url='https://github.com/Adamtaranto/teloclip',
+      author='Adam Taranto',
+      author_email='aptaranto@ucdavis.edu',
+      license='MIT',
+      packages=['teloclip'],
+      classifiers=pypi_classifiers,
+      keywords=["samfile","telomere","alignment"],
+      include_package_data=True,
+      zip_safe=False,
+      entry_points={
+        'console_scripts': [
+            'teloclip=teloclip.run_self:main',
+        ],
+    },
+    )
