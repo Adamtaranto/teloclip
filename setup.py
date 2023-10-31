113f15907172eb26c88de47875061e7bd5b9b8fb
Adam Taranto <adam.p.taranto@gmail.com>
2023-10-31 15:12:47 +1100

    Init pyproject files

--- setup.py
+++ setup.py
@@ -1,35 +1,4 @@
 from setuptools import setup
 
-pypi_classifiers = [
-    'Programming Language :: Python :: 3',
-    "Development Status :: 4 - Beta",
-    "Environment :: Console",
-    "Operating System :: OS Independent",
-    'Intended Audience :: Science/Research',
-    'Natural Language :: English',
-    'Topic :: Scientific/Engineering :: Bio-Informatics',
-    "Topic :: Software Development :: Libraries :: Python Modules",
-    'License :: OSI Approved :: MIT License',
-]
-
-desc = """Filter SAM file for soft-clipped alignments containing unassembled telomeric repeats."""
-
-setup(name='teloclip',
-      version='0.0.3',
-      description=desc,
-      url='https://github.com/Adamtaranto/teloclip',
-      author='Adam Taranto',
-      author_email='aptaranto@ucdavis.edu',
-      license='MIT',
-      packages=['teloclip'],
-      classifiers=pypi_classifiers,
-      keywords=["samfile","telomere","alignment"],
-      include_package_data=True,
-      zip_safe=False,
-      entry_points={
-        'console_scripts': [
-            'teloclip=teloclip.run_self:main',
-            'teloclip-extract=teloclip.run_extract:main',
-        ],
-    },
-    )
+if __name__ == "__main__":
+    setup()
