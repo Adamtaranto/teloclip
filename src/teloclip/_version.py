ea14130d92abc30ef2648f09fb77c2ac4b615f63
Adam Taranto <adam.p.taranto@gmail.com>
2023-11-04 00:25:29 +1100

    test update version

--- src/teloclip/_version.py
+++ src/teloclip/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.0.5.dev1+g0275ffd.d20231103'
-__version_tuple__ = version_tuple = (0, 0, 5, 'dev1', 'g0275ffd.d20231103')
+__version__ = version = '0.0.5.dev3+g6b09183'
+__version_tuple__ = version_tuple = (0, 0, 5, 'dev3', 'g6b09183')
