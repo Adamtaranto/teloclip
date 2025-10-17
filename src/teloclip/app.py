822e713daaf4eb297ca26cd0c7455326f07049ea
Adam Taranto <adam.p.taranto@gmail.com>
2025-10-17 17:56:20 +1100

    Use fuzzy motifs and format arg names correctly

--- src/teloclip/app.py
+++ src/teloclip/app.py
@@ -19,6 +19,7 @@ import sys
 
 from teloclip._version import __version__
 from teloclip.logs import init_logging
+from teloclip.motifs import make_fuzzy_motif_regex, make_motif_regex
 from teloclip.samops import processSamlines
 from teloclip.seqops import addRevComplement, read_fai
 
@@ -33,19 +34,22 @@ def mainArgs():
         'samfile', nargs='?', type=argparse.FileType('r'), default=sys.stdin
     )
     parser.add_argument(
-        '--refIdx',
+        '--ref-idx',
+        dest='ref_idx',
         type=str,
         required=True,
         help='Path to fai index for reference fasta. Index fasta using `samtools faidx FASTA`',
     )
     parser.add_argument(
-        '--minClip',
+        '--min-clip',
+        dest='min_clip',
         type=int,
         default=1,
         help='Require clip to extend past ref contig end by at least N bases.',
     )
     parser.add_argument(
-        '--maxBreak',
+        '--max-break',
+        dest='max_break',
         type=int,
         default=50,
         help='Tolerate max N unaligned bases before contig end.',
@@ -54,11 +58,12 @@ def mainArgs():
         '--motifs',
         type=str,
         default=None,
-        help='If set keep only reads containing given motif/s from comma delimited list of strings. \
-                        By default also search for reverse complement of motifs. i.e. TTAGGG,TTAAGGG will also match CCCTAA,CCCTTAA',
+        help='If set keep only reads containing given motif/s from comma delimited list of strings. '
+        'By default also search for reverse complement of motifs. i.e. TTAGGG,TTAAGGG will also match CCCTAA,CCCTTAA',
     )
     parser.add_argument(
-        '--noRev',
+        '--no-rev',
+        dest='no_rev',
         default=False,
         action='store_true',
         help='If set do NOT search for reverse complement of specified motifs.',
@@ -67,27 +72,30 @@ def mainArgs():
         '--fuzzy',
         default=False,
         action='store_true',
-        help='If set, tolerate +/- 1 variation in motif homopolymer runs \
-            i.e. TTAGGG -> T{1,3}AG{2,4}. Default: Off',
+        help='If set, tolerate +/- 1 variation in motif homopolymer runs '
+        'i.e. TTAGGG -> T{1,3}AG{2,4}. Default: Off',
     )
     parser.add_argument(
         '-r',
-        '--min_repeats',
-        default=3,
+        '--min-repeats',
+        dest='min_repeats',
+        default=1,
         type=int,
         help='Minimum number of sequential pattern matches required for a hit to be reported. Default: 3',
     )
     parser.add_argument(
-        '--noPoly',
-        default=False,
-        action='store_true',
-        help='WARNING: OPTION DEPRECIATED, USE --FUZZY. Default: False',
+        '--min-anchor',
+        dest='min_anchor',
+        default=500,
+        type=int,
+        help='Minimum number of aligned bases (anchor) required on the non-clipped portion of the read. Default: 500',
     )
     parser.add_argument(
-        '--matchAnywhere',
+        '--match-anywhere',
+        dest='match_anywhere',
         default=False,
         action='store_true',
-        help='If set, motif match may occur in unclipped region of alignment.',
+        help='If set, motif match may occur in unclipped region of reads.',
     )
     parser.add_argument(
         '-v',
@@ -96,6 +104,7 @@ def mainArgs():
         version='%(prog)s {version}'.format(version=__version__),
     )
     args = parser.parse_args()
+
     return args
 
 
@@ -107,42 +116,41 @@ def main():
     args = mainArgs()
 
     # Fetch contig lens
-    ContigDict = read_fai(args.refIdx)
+    contig_dict = read_fai(args.ref_idx)
 
-    # Split motifs into list
     if args.motifs:
+        # Create list of motifs from comma delimited string
         motifList = args.motifs.split(',')
+
         # Add rev comp motifs to list, make unique set.
-        if not args.noRev:
+        if not args.no_rev:
             motifList = addRevComplement(motifList)
+
+        # Make unique set.
+        motifList = list(set(motifList))
+
+        # Create regex patterns for each motif
         if args.fuzzy:
-            motifList = motifList  # TODO: Implement convert to regex
-            # convert to regex pattern(motifList)
+            # Create fuzzy regex patterns for each motif
+            logging.info(
+                'Using option --fuzzy to find inexact motif matches. Tolerate +/- 1 base variance in motif homopolymers.'
+            )
+            motifList = [make_fuzzy_motif_regex(motif) for motif in motifList]
+        else:
+            # Create exact regex patterns for each motif
+            motifList = [make_motif_regex(motif) for motif in motifList]
     else:
         motifList = []
 
-    # Log depreciation warning if using --noPoly option
-    if args.noPoly:
-        logging.warning(
-            'WARNING: Option --noPoly is depreciated and should not be used. \
-            Switching to option --fuzzy instead to find inexact motif matches.'
-        )
-        args.fuzzy = True
-
-    if args.fuzzy:
-        logging.info(
-            'INFO: Using option --fuzzy to find inexact motif matches. \
-                Tolerate +/- 1 base variance in motif homopolymers.'
-        )
+    # Process SAM lines
 
     processSamlines(
         args.samfile,
-        ContigDict,
+        contig_dict,
         motifList,
-        matchAnywhere=args.matchAnywhere,
-        maxBreak=args.maxBreak,
-        minClip=args.minClip,
-        noRev=args.noRev,
-        fuzzy=args.fuzzy,
-        minRepeats=args.min_repeats,
+        match_anywhere=args.match_anywhere,
+        max_break=args.max_break,
+        min_clip=args.min_clip,
+        min_repeats=args.min_repeats,
+        min_anchor=args.min_anchor,
     )
