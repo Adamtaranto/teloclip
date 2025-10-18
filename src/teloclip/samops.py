cfd26225e151339a44fd332af94dcab29e38daad
Adam Taranto <adam.p.taranto@gmail.com>
2025-10-18 12:49:36 +1100

    mv streamingsplitbycontig to samops.py

--- src/teloclip/samops.py
+++ src/teloclip/samops.py
@@ -1,9 +1,10 @@
 import logging
 import re
 import sys
+import os
 
 from teloclip.motifs import check_sequence_for_patterns
-from teloclip.seqops import isMotifInClip
+from teloclip.seqops import isMotifInClip, writefasta
 
 # TODO:
 """
@@ -301,6 +302,86 @@ def StreamingSamFilter(samfile=None, contigs=None, max_break=50, min_clip=1):
                             'R',
                         )
                     )
+
+
+def StreamingSplitByContig(alignments=None, contigs=None, prefix=None, outdir=None):
+    """
+    Takes alignment line summaries tagged with overhang information from StreamingSamFilter
+    Writes reads into output files for each end of each contig with at least one alignment found.
+    """
+    # Note: This method opens and closes the output fasta files for EVERY read processed.
+    # Probably inefficient but avoids reading everything into memory
+    if outdir:
+        if not os.path.isdir(outdir):
+            os.makedirs(outdir)
+    else:
+        outdir = os.getcwd()
+    # Output file tracker
+    outpaths = []
+    # Counter
+    readCount = 0
+    # For each aligned read
+    for read in alignments:
+        readCount += 1
+        # Log update every 10K reads
+        if not readCount % 10000:
+            logging.info('Alignments processed: %s' % str(readCount))
+        # Check if alignment is at right end overhang
+        if read[6] == 'R':
+            # i.e [(alnStart,alnEnd,rightClipLen,readSeq,readName,contigName,'R')]
+            if prefix:
+                base = '_'.join([prefix, str(read[5])])
+            else:
+                base = str(read[5])
+            # Set output file path
+            outfileR = os.path.join(outdir, '_'.join([base, 'R']) + '.fasta')
+            # Check if need to create new outfile or append to existing file.
+            if outfileR not in outpaths:
+                logging.info('Creating new outfile: %s' % str(outfileR))
+                outpaths.append(outfileR)
+                # Output reads aligned to right end of contig
+                with open(outfileR, 'w') as fileR:
+                    # Convert overhanging segment of read to lowercase
+                    masked = read[3][: -read[2]] + read[3][-read[2] :].lower()
+                    # Write masked read to fasta
+                    writefasta(fileR, str(read[4]), masked)
+            else:
+                # If file already exists
+                # Output reads aligned to right end of contig
+                with open(outfileR, 'a') as fileR:
+                    # Convert overhanging segment of read to lowercase
+                    masked = read[3][: -read[2]] + read[3][-read[2] :].lower()
+                    # Write masked read to fasta
+                    writefasta(fileR, str(read[4]), masked)
+        # Check if alignment is a left end overhang
+        elif read[6] == 'L':
+            # i.e [(alnStart,alnEnd,leftClipLen,readSeq,readname,contigname,'L')]
+            if prefix:
+                base = '_'.join([prefix, str(read[5])])
+            else:
+                base = str(read[5])
+            # Set output file path
+            outfileL = os.path.join(outdir, '_'.join([base, 'L']) + '.fasta')
+            # Check if need to create new outfile or append to existing file.
+            if outfileL not in outpaths:
+                logging.info('Creating new outfile: %s' % str(outfileL))
+                outpaths.append(outfileL)
+                # Output reads aligned to left end of contig
+                with open(outfileL, 'w') as fileL:
+                    # Convert overhanging segment of read to lowercase
+                    masked = read[3][: read[2]].lower() + read[3][read[2] :]
+                    # Write masked read to fasta
+                    writefasta(fileL, str(read[4]), masked)
+            else:
+                # If file already exists
+                # Output reads aligned to left end of contig
+                with open(outfileL, 'a') as fileL:
+                    # Convert overhanging segment of read to lowercase
+                    masked = read[3][: read[2]].lower() + read[3][read[2] :]
+                    # Write masked read to fasta
+                    writefasta(fileL, str(read[4]), masked)
+
+    logging.info('Total alignments processed: %s' % str(readCount))
 
 
 def SAMinfo():
